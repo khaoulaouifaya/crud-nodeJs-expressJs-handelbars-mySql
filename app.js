5f7cb7b140cd206eeed83c2ec33adf212ab47153
khaoula ouifaya <khaoula.ouifaya@centoria.fr>
2023-11-25 17:32:50 +0100

    node js with express and pug template engine

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
 import express from 'express';
-import handlebars from 'express-handlebars';
+import {getAll} from './database.js';
 import path from 'path';
-
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-
-
-const port = 3000;
-const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 
 
+const port = 3000;
+const app = express();
+const router = express.Router();
 
-app.engine('hbs', handlebars.engine({
-    layoutsDir: __dirname + '/views/layouts',
-    extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.set('view engine', 'pug');
+app.set('views',path.join(__dirname, 'views'))
+
 app.listen(port, () => console.log(`App listening to port ${port}`));
 
-app.get('/', (req, res) => {
-    res.render('main', { layout: 'index' });
+app.get('/',async (req, res) => {
+    var all = await getAll();
+    res.render('main',{data:JSON.stringify(all)});
 });
+
+app.get('/users',(req,res)=>{
+    const datab = {
+        pageTitle: 'Home Page',
+        message: 'Welcome to our website!'
+    }
+    res.render('main',{data: datab});
+})
